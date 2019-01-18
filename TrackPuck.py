59bacc21b12bdec50920160ffb3695962d4db849
Yang Yue <yyue1@andrew.cmu.edu>
2019-01-18 02:13:10 -0500

    new tracker and velocity

--- TrackPuck.py
+++ TrackPuck.py
@@ -1,53 +1,26 @@
 import cv2
 import numpy as np
+import time
 
-def nothing(temp):
-    return
+class Puck(object):
+    def __init__(self):
+        self.frame = None
+        self.x = None
+        self.y = None
+        self.dy = 0
+        self.dx = 0
+        self.time = 0
 
-def track():
-    rgbMax = 255
-    rgbMin = 0
+    def track(self):
+        upperBound = np.array([[[255, 255, 255]]])
+        lowerBound = np.array([[[103, 113, 91]]])
 
-    sliderForPuck = "Slider for Puck"
-    cv2.namedWindow(sliderForPuck)
-
-    cam_index = 1 # Default camera is at index 0.
-    cap = cv2.VideoCapture(cam_index) # Video capture object
-
-    sliders = ['R_MAX', 'G_MAX', 'B_MAX', 'R_MIN', 'G_MIN', 'B_MIN']
-
-    for slider in sliders:
-        cv2.createTrackbar(slider, sliderForPuck, rgbMin, rgbMax, nothing)
-
-    # Sliders for tracking Puck. Set default values after getting the puck!!
-    cv2.setTrackbarPos("R_MAX", sliderForPuck, 136)
-    cv2.setTrackbarPos("R_MIN", sliderForPuck, 103)
-    cv2.setTrackbarPos("G_MAX", sliderForPuck, 234)
-    cv2.setTrackbarPos("G_MIN", sliderForPuck, 113)
-    cv2.setTrackbarPos("B_MAX", sliderForPuck, 162)
-    cv2.setTrackbarPos("B_MIN", sliderForPuck, 91)
-
-    cap.open(cam_index) # Enable the camera
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            cap.release()
-            break
-        ret, frame = cap.read()
-
-        thresholds = []
-        for slider in sliders:
-            thresholds.append(cv2.getTrackbarPos(slider, sliderForPuck))
-
-        upperBound = np.array(thresholds[0:3])
-        lowerBound = np.array(thresholds[3:6])
-
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
         threshold = cv2.inRange(hsv, lowerBound, upperBound)
         threshold = cv2.erode(threshold, None, iterations=2)
         threshold = cv2.dilate(threshold, None, iterations=2)
 
-        res = cv2.bitwise_and(frame, frame, mask = threshold)
+        res = cv2.bitwise_and(self.frame, self.frame, mask = threshold)
 
         blurred = cv2.GaussianBlur(res, (5, 5), 0)
         canny = cv2.Canny(blurred, 230, 255)
@@ -57,16 +30,25 @@ def track():
         if len(contours) > 0:
             c = max(contours, key = cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.rectangle(res,(x,y),(x+w,y+h),(0,255,0),2)
-            centerOfPuck = (int((2*x + w)//2), int((2*y + h)//2))
-            cv2.circle(frame, centerOfPuck, 1, (0, 255, 0), 10)
-            cv2.circle(res, centerOfPuck, 1, (0, 255, 0), 10)
-            centerOfPuck = ((x+w)//2, (y+h)//2)
-            print(centerOfPuck)
-
-        #displays all windows
-        cv2.imshow('Input', frame)
-        cv2.imshow("Puck", res)
-
-track()
+            curr_time = time.time()
+            cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,255,0),2)
+            (curr_x, curr_y) = (int((2*x + w)//2), int((2*y + h)//2))
+            cv2.circle(self.frame, (curr_x, curr_y), 1, (0, 255, 0), 10)
+            if self.x == None:
+                self.x = curr_x
+                self.y = curr_y
+            move_x = curr_x - self.x
+            move_y = curr_y - self.y
+            time_diff = curr_time - self.time
+            print(time_diff)
+            if time_diff != 0:
+                self.dx = move_x / time_diff
+                self.dy = move_y / time_diff
+            self.time = curr_time
+            self.x = curr_x
+            self.y = curr_y
+        print("X velocity: ", self.dx)
+        print("Y velocity: ", self.dy)
+            #displays all windows
+        cv2.imshow('Input', self.frame)
+        #    cv2.imshow("Puck", res)
--- table_pers.py
+++ table_pers.py
@@ -1,27 +1,30 @@
 import cv2 as cv
 import numpy as np
-#import puck_tracker2
+from TrackPuck import Puck
 
 cap = cv.VideoCapture(1);
 cap.open(1)
+PuckTracker = Puck()
+
 def track_table():
     _, frame = cap.read()
     #frame = cv.imread("./table3.jpg")
-    cv.circle(frame, (30, 50), 5, (0, 0, 255), -1)
-    cv.circle(frame, (750, 100), 5, (0, 0, 255), -1)
-    cv.circle(frame, (30, 475), 5, (0, 0, 255), -1)
-    cv.circle(frame, (750, 550), 5, (0, 0, 255), -1)
+    cv.circle(frame, (203, 104), 5, (0, 0, 255), -1)
+    cv.circle(frame, (479, 90), 5, (0,  0, 255), -1)
+    cv.circle(frame, (527, 466), 5, (0, 0, 255), -1)
+    cv.circle(frame, (180, 480), 5, (0, 0, 255), -1)
 
-    pts1 = np.float32([[30, 50], [750, 100], [30, 475], [750, 550]])
-    pts2 = np.float32([[0, 0], [800, 0], [0, 400], [800, 400]])
+    pts1 = np.float32([[203, 104], [479, 90], [527, 466], [180, 480]])
+    pts2 = np.float32([[0, 380], [0, 0], [460, 0], [460, 380]])
     matrix = cv.getPerspectiveTransform(pts1, pts2)
 
-    result = cv.warpPerspective(frame, matrix, (800, 400))
+    result = cv.warpPerspective(frame, matrix, (460, 360))
 
     cv.imshow("Frame", frame)
     cv.imshow("Perspective transformation", result)
 
-    #puck_tracker2.track_puck(result)
+    PuckTracker.frame = result
+    PuckTracker.track()
     key = cv.waitKey(1)
     if key == 27:
         return
