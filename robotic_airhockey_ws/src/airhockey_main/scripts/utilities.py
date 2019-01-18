6983cc975f7fa7ecc1a2f3932205c1eda0282036
Alvinosaur <ashek@andrew.cmu.edu>
2019-01-18 00:36:26 -0500

    Added safety checks for calling different robot strategies

--- robotic_airhockey_ws/src/airhockey_main/scripts/utilities.py
+++ robotic_airhockey_ws/src/airhockey_main/scripts/utilities.py
@@ -49,3 +49,33 @@ def transform(value, is_pos, y_bound=None):
             raise MissingParamError('Missing y_bound for position transform')
     else: return -1 * value
 
+
+def pass_vel_threshold(vel_y, table_length, table_proportion):
+    """
+    Checks if puck's velocity y-component is above a certain magnitude threshold. If y-velocity too low,
+    will actually cause recursion stack overflow. Note: Please make sure to normalize the velocity with
+    some reasonable time unit like seconds. If you use milliseconds or smaller, this threshold will not work.
+
+    """
+    assert(0 < table_proportion < 1)
+    return abs(vel_y) > table_proportion * table_length
+
+
+def past_table_bounds(puck_y, table_length):
+    """
+    Checks if puck's y position is out of range of the table. Of course this won't happen ever, but
+    this checks when the puck should have intersected with either length-wise end of the table.
+    """
+    return 0 <= puck_y <= table_length
+
+
+def is_puck_approaching(puck_vy):
+    """
+    Seemingly simple, but important check so avoid doing arm-puck collision calculations
+    if puck isn't coming to arm. Also avoids other unforeseen problems with calculations.
+
+    NOTE: This is defined with the cartesian coordinate system where the robot arm is located at y = 0,
+    so the puck is only moving toward the arm if its velocity is negative.
+
+    """
+    return puck_vy < 0
