01b71f4cb52ec39f82be6ed59826ab37dc551542
Alvinosaur <ashek@andrew.cmu.edu>
2019-06-25 08:43:52 -0700

    add parsing of json file for filter params

--- puck_tracker2.py
+++ puck_tracker2.py
@@ -54,62 +54,59 @@ while True:
 '''
 
 import cv2 as cv
+import json
 import numpy as np
 
-CAM_INDEX = 1; #default camera index is 0
+CAM_INDEX = 1; # default camera index is 0
+OBJ_HIGHLIGHT_COLOR = (0, 255, 0)  # Green
+OBJ_HIGHLIGHT_LINE_WIDTH = 2
+OBJ_CENTER_COLOR = (255, 0, 0)  # RED
 
-cap = cv.VideoCapture(CAM_INDEX);
-cap.open(CAM_INDEX) #Open the camera
 
-def track_puck(cam):
+def track_puck(cam, thresholds):
     if cv.waitKey(1) & 0xFF == ord("q"):
         cv.destroyAllWindows()
         cam.release()
         return
-    ret, frame = cam.read()
+    _, frame = cam.read()
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    #we define the green for the convert to filter in HSV
-
-    lower_red = np.array([25, 189, 118])
-    upper_red = np.array([95, 155, 198])
-    #lower_red=np.array([[[33,80,40]]],np.uint8)
-    #upper_red=np.array([[[86,255,255]]],np.uint8)
-    #lower_red = np.array([33, 80, 40])
-    #upper_red = np.array([102, 255, 255])
-    #lower_red_hsv = cv.cvtColor(lower_red,cv.COLOR_BGR2HSV)
-    #upper_red_hsv = cv.cvtColor(upper_red,cv.COLOR_BGR2HSV)
-
     #applying a mask to get only lower_green
-    mask = cv.inRange(hsv, lower_red, upper_red)
-    #mask = red_mask
-    #mask = cv.cvtColor(hsv, (36, 25, 25), (78, 255, 255))
-    #mask = cv.inRange(hsv, [[[112-10, 90-10, 41-10]]], [[[112+10, 90+10, 41+10]]])
-    res = cv.bitwise_and(frame, frame, mask = mask)
-    cv.imshow('frame', frame)
-    #cv.imshow('mask', mask2)
-    cv.imshow('res', res)
+    for obj_name in thresholds.keys():
+        thresholds[obj_name]['mask'] = cv.inRange(hsv, 
+            np.array(thresholds[obj_name]['lower']), 
+            np.array(thresholds[obj_name]['upper']))
 
-    #find blue puck using edge detection and contours
-    blurred = cv.GaussianBlur(res, (5, 5), 0)
-    canny = cv.Canny(blurred, 230, 255)
-    cv.imshow("Canny", canny)
+        filtered_img = cv.bitwise_and(frame, frame, mask = thresholds[obj_name]['mask'])
 
-    im2, contours, hierarchy = cv.findContours(canny.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # Find target using edge detection and contours
+        blurred = cv.GaussianBlur(filtered_img, (5, 5), 0)
+        canny = cv.Canny(blurred, 230, 255)
+        _, contours, hierarchy = cv.findContours(canny.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    frame2 = frame.copy()
-    cv.drawContours(frame2, contours, -1, (0, 255, 0), 2)
-    cv.imshow("Contours", frame2)
+        # -1 to draw all detected contours
+        cv.drawContours(frame, contours, -1, OBJ_HIGHLIGHT_COLOR, OBJ_HIGHLIGHT_LINE_WIDTH)
 
-    #find center of puck if contour exists
-    if len(contours) is not 0:
-        c = max(contours, key = cv.contourArea)
-        x, y, w, h = cv.boundingRect(c)
-        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-        centerOfPuck = (int((2*x + w)//2), int((2*y + h)//2))
-        cv.circle(frame, centerOfPuck, 1, (0, 255, 0), 10)
+        #find center of puck if contour exists
+        if len(contours) is not 0:
+            c = max(contours, key = cv.contourArea)
+            x, y, w, h = cv.boundingRect(c)
+            cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            centerOfPuck = (int((2*x + w)//2), int((2*y + h)//2))
+            cv.circle(frame, centerOfPuck, 1, OBJ_CENTER_COLOR, 10)
 
-    cv.imshow("Objects", frame)
+        cv.imshow(obj_name, frame)
+
+try:
+    with open('json_files/object_hsv_filters.json', 'r') as f:
+        object_hsv_params = json.load(f)
+except IOError:
+    print('Need to calibrate camera first! Please run \'calibrate_camera.py\'')
+    exit(-1)
+
+# Open the camera
+cap = cv.VideoCapture(CAM_INDEX)
+cap.open(CAM_INDEX)
 
 while True:
-    track_puck(cap)
+    track_puck(cap, object_hsv_params)
